d011d3c591a6f11013a592fd540372d0f0ab1fcf
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2017-03-19 18:31:46 +0200

    data download script, can't commit 25gb of stuff directly

---
+++ data/get_data.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash 
+
+wget http://data.dws.informatik.uni-mannheim.de/hyperlinkgraph/2014-03/webgraph/hostgraph.graph
+wget http://data.dws.informatik.uni-mannheim.de/hyperlinkgraph/2014-03/webgraph/hostgraph.offsets
+wget http://data.dws.informatik.uni-mannheim.de/hyperlinkgraph/2014-03/webgraph/hostgraph.properties
+wget http://data.dws.informatik.uni-mannheim.de/hyperlinkgraph/2014-03/webgraph/pldgraph.graph
+wget http://data.dws.informatik.uni-mannheim.de/hyperlinkgraph/2014-03/webgraph/pldgraph.offsets
+wget http://data.dws.informatik.uni-mannheim.de/hyperlinkgraph/2014-03/webgraph/pldgraph.properties
+wget http://data.dws.informatik.uni-mannheim.de/hyperlinkgraph/2014-03/webgraph/webgraph.graph
+wget http://data.dws.informatik.uni-mannheim.de/hyperlinkgraph/2014-03/webgraph/webgraph.offsets
+wget http://data.dws.informatik.uni-mannheim.de/hyperlinkgraph/2014-03/webgraph/webgraph.properties
