cbdff90564844dd9c5c3f9e8b68b6cf7ee0b66b8
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2017-03-30 15:12:25 +0300

    bibliograpy config

--- docs/build.sh
+++ docs/build.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+rm -f sample.log
+docker run --rm -v $(pwd):/docs ktulatex xelatex --shell-escape sample.tex     
+docker run -it --rm -v $(pwd):/docs ktulatex biber sample  
 docker run --rm -v $(pwd):/docs ktulatex xelatex --shell-escape sample.tex     
