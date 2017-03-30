d78d6bff3ddd24468c2ca1bf05622d8970c8708a
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2017-03-30 15:25:15 +0300

    best biuld script ever

--- docs/build.sh
+++ docs/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-rm -f sample.log
-docker run --rm -v $(pwd):/docs ktulatex xelatex --shell-escape sample.tex     
-docker run -it --rm -v $(pwd):/docs ktulatex biber sample  
-docker run --rm -v $(pwd):/docs ktulatex xelatex --shell-escape sample.tex     
+rm -f paper.log
+docker run --rm -v $(pwd):/docs ktulatex xelatex --shell-escape paper.tex
+docker run -it --rm -v $(pwd):/docs ktulatex biber paper
+docker run --rm -v $(pwd):/docs ktulatex xelatex --shell-escape paper.tex
+while grep -q "Please rerun LaTeX" paper.log
+do
+    rm -f paper.log
+    docker run --rm -v $(pwd):/docs ktulatex xelatex --shell-escape paper.tex
+done
