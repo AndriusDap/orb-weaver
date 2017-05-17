c1aaba94c8bf236fb7047e42f0cb0c4a920650c3
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2017-05-17 10:19:03 +0300

    starting with pagerank, need  to turn the graph around

--- apps/decompress/src/main/scala/andriusdap/orbweaver/decompress/App.scala
+++ apps/decompress/src/main/scala/andriusdap/orbweaver/decompress/App.scala
@@ -1,6 +1,9 @@
 package andriusdap.orbweaver.decompress
 
-import it.unimi.dsi.big.webgraph.ArcListASCIIGraph
+import java.lang.Long
+import java.util.function.Consumer
+
+import it.unimi.dsi.big.webgraph.{ArcListASCIIGraph, BVGraph}
 
 object App {
   def main(args: Array[String]): Unit = {
@@ -12,8 +15,45 @@ object App {
         }
     }.get
 
-    ArcListASCIIGraph.main(Array(
-      "-g", "BVGraph", dir, dir + ".edges"
-    ))
+    val graph = BVGraph.load(dir)
+
+    require(graph.numNodes() < Int.MaxValue, s"Cannot handle more than maxint nodes, received ${graph.numNodes()}")
+
+    var scores = Array.fill[Float](graph.numNodes().toInt)(1.0f)
+
+    var newScores = new Array[Float](graph.numNodes().toInt)
+
+    val dampening = 0.85f
+    val oneMinusDampening = 1 - dampening
+
+    val before = System.currentTimeMillis()
+    println(s"starting first iteration ${System.currentTimeMillis()}")
+    graph.nodeIterator(0).forEachRemaining (
+      new Consumer[Long] {
+        override def accept(t: Long): Unit = {
+          val successors = graph.successorBigArray(t)
+
+          var sum = 0.0f
+
+          var i = 0
+          while(i < successors.length) {
+            var j = 0
+
+            while(j < successors(i).length) {
+              val currentl = successors(i)(j)
+              val current = currentl.toInt
+              sum += scores(current)/graph.outdegree(currentl)
+              j += 1
+            }
+
+            i += 1
+          }
+
+          scores(t.toInt) = oneMinusDampening + dampening * sum
+        }
+      }
+    )
+    println(scores.take(100).mkString("\n"))
+    println(s"done with iteration in ${System.currentTimeMillis() - before}ms")
   }
 }
