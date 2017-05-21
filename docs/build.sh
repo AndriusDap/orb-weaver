eb38121683e94c4677cb51c841c38e62f7e5a1e4
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2017-05-21 20:21:57 +0300

    refs to lists

--- docs/build.sh
+++ docs/build.sh
@@ -5,7 +5,11 @@ docker run --rm -v $(pwd):/docs ktulatex xelatex --shell-escape --output-directo
 
 if grep -q "Please (re)run Biber on the file" out/paper.log
 then
+    echo "==========================================================================================="
+    echo "RUNNING BIBER"
     docker run --rm -v $(pwd):/docs ktulatex biber --input-directory out --output-directory out paper
+    echo "DONE WITH BIBER"
+    echo "==========================================================================================="
     docker run --rm -v $(pwd):/docs ktulatex xelatex --shell-escape --output-directory=./out paper.tex
 fi
 
