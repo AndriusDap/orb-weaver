785adbd3d3faae04a38959c0b67ad4aa206e977c
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2017-05-23 17:47:07 +0300

    fucking rest

--- apps/frontend/src/main/scala/andriusdap/orbweaver/controllers/Clasifier.scala
+++ apps/frontend/src/main/scala/andriusdap/orbweaver/controllers/Clasifier.scala
@@ -2,25 +2,33 @@ package andriusdap.orbweaver.controllers
 
 import com.google.inject.Singleton
 import play.api.Logger
+import java.io.File
+
 import play.api.mvc._
 import andriusdap.orbweaver.executor.App
 import andriusdap.orbweaver.executor.UrlBuilder
 import andriusdap.orbweaver.executor.App.FeatureSet
+
 import sys.process._
 import scala.io.{Codec, Source}
-
 import scala.concurrent.Future
+import scala.language.postfixOps
 import scala.util.{Failure, Success, Try}
 
 @Singleton
 class Clasifier {
   lazy val maliciousRanks = loadPageranks("/media/ssd/temp_dir/malicious_pagerank.txt")
   lazy val benignRanks = loadPageranks("/media/ssd/temp_dir/benign_pagerank.txt")
+  val model = "/media/ssd/vw/model.vw"
+
   lazy val minMalicious = maliciousRanks.values.min
   lazy val minBenign = benignRanks.values.min
 
   def loadPageranks(source: String): Map[String, Float] = {
-    Source.fromFile(source).getLines().map(_.split(" ")).map(c => c(0) -> c(1).toFloat).toMap
+    Source.fromFile(source).getLines().map{c =>
+      val s = c.split(" ")
+      s(0) -> s(1).toFloat
+    }.toMap
   }
 
   def classify(source: String) = Action {
@@ -39,9 +47,9 @@ class Clasifier {
         benign,
         App.Benign
       ))
+//s"vw -t -i $model -p /dev/stdout --quiet"
+      val result = Process("echo", Seq(sample)) #| Process("/media/ssd/vw/test.sh") lineStream_!
 
-      var builder = ""
-      val a = "echo labas" !(ProcessLogger(builder += _))
-      Results.Ok("true" + builder.toString)
+      Results.Ok(result.head)
   }
 }
